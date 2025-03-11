5253efd4cec4e8d321ab3832e5121fed66d916b8
guidoasbun <guido@asbun.io>
2025-03-11 16:47:15 -0700

    works maybe?

--- game_of_nim.py
+++ game_of_nim.py
@@ -1,28 +1,47 @@
 from games import *
 
 class GameOfNim(Game):
-    """Play Game of Nim with first player 'MAX'.
-    A state has the player to move, a cached utility, a list of moves in
-    the form of a list of (x, y) positions, and a board, in the form of
-    a list with number of objects in each row."""
 
-    def __init__(self, board=[3,1]):
-        raise NotImplementedError
+    def __init__(self, board=None):
+        if board is None:
+            board = [7, 5, 3, 1]
+
+        self.initial = GameState(to_move='MAX', utility=0, board=board, moves=None)
 
     def actions(self, state):
-        """Legal moves are at least one object, all from the same row."""
-        return state.moves
+        moves = []
+        for row in range(len(state.board)):
+            for num_objects in range(1, state.board[row] + 1):
+                moves.append((row, num_objects))
+        return moves
 
     def result(self, state, move):
-        raise NotImplementedError
+        board = state.board.copy()
+        row, num_objects = move
+
+        if row < 0 or row >= len(board) or num_objects > board[row]:
+            return state
+        
+        board[row] -= num_objects
+
+        next_player = 'MIN' if state.to_move == 'MAX' else 'MAX'
+
+        utility = 0
+        if sum(board) == 0:
+            utility = -1 if next_player == 'MAX' else 1
+
+        return GameState(to_move=next_player,
+                        utility=utility,
+                        board=board,
+                        moves=None)
 
     def utility(self, state, player):
         """Return the value to player; 1 for win, -1 for loss, 0 otherwise."""
-        raise NotImplementedError
+        return state.utility if player == 'MAX' else -state.utility
 
     def terminal_test(self, state):
         """A state is terminal if there are no objects left"""
-        raise NotImplementedError
+        return sum(state.board) == 0
 
     def display(self, state):
         board = state.board
@@ -30,8 +49,8 @@ class GameOfNim(Game):
 
 
 if __name__ == "__main__":
-    nim = GameOfNim(board=[0, 5, 3, 1]) # Creating the game instance
-    #nim = GameOfNim(board=[7, 5, 3, 1]) # a much larger tree to search
+    #nim = GameOfNim(board=[0, 5, 3, 1]) # Creating the game instance
+    nim = GameOfNim(board=[7, 5, 3, 1]) # a much larger tree to search
     print(nim.initial.board) # must be [0, 5, 3, 1]
     print(nim.initial.moves) # must be [(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (2, 1), (2, 2), (2, 3), (3, 1)]
     print(nim.result(nim.initial, (1,3) ))
