44f14e181d0ad6932b1a6b24057166a914c2be2e
guidoasbun <guido@asbun.io>
2025-03-12 16:49:51 -0700

    updates

--- game_of_nim.py
+++ game_of_nim.py
@@ -53,11 +53,12 @@ class GameOfNim(Game):
         return GameState(to_move=next_player,
                         utility=utility,
                         board=board,
-                        moves=None)
+                        moves=new_moves)
 
     def utility(self, state, player):
         """Return the value to player; 1 for win, -1 for loss, 0 otherwise."""
-        return state.utility if player == 'MAX' else -state.utility
+        # return state.utility if player == 'MAX' else -state.utility
+        return 1 if state.utility == 1 and player == 'MAX' else -1 if state.utility == -1 and player == 'MIN' else 0
 
     def terminal_test(self, state):
         """A state is terminal if there are no objects left"""
@@ -71,11 +72,12 @@ class GameOfNim(Game):
 if __name__ == "__main__":
     #nim = GameOfNim(board=[0, 5, 3, 1]) # Creating the game instance
     nim = GameOfNim(board=[7, 5, 3, 1]) # a much larger tree to search
+    result_state = nim.result(nim.initial, (1,2)) # computer moves first
     print(nim.initial.board) # must be [0, 5, 3, 1]
     print(nim.initial.moves) # must be [(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (2, 1), (2, 2), (2, 3), (3, 1)]
-    print(nim.result(nim.initial, (1,3) ))
+    print(nim.result(nim.initial, (1,3)).board)
     utility = nim.play_game(alpha_beta_player, query_player) # computer moves first 
-    if (utility < 0):
+    if utility < 0:
         print("MIN won the game")
     else:
         print("MAX won the game")
